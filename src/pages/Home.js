040cdc4e843e88f3b5a2255dd604e3b88d7dfed9
Willon <willon.freitas@tm2digital.com>
2021-12-16 22:20:59 -0300

    Atualização codigo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "../components/wallet";
 import TokenListRinkeby from "../assets/token-list-rinkeby.json";
-import { useEffect} from "react";
+import { useEffect } from "react";
 import useBalance from "../actions/useBalance.js";
 
 export function Home() {
   const { activate, account } = useWeb3React();
+  const [balances] = useBalance(TokenListRinkeby, 18)
 
-  const [balances] = useBalance(TokenListRinkeby, 18);
-
+  async function connect() {
+    try {
+      activate(injected);
+    } catch {
+      console.log("Metamask not installed");
+    }
+  }
   function copy(text) {
     var input = document.createElement("input");
     document.body.append(input);
@@ -29,22 +35,19 @@ export function Home() {
     );
   }, [balances]);
   return (
-    <div>
+    <div className="full-height">
       <header>
         <div className="container ">
           <div className="d-flex justify-content-around align-items-center">
             <div>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/scam128.png"}
+            <img
+                src={process.env.PUBLIC_URL + "/assets/LABRAS.png"}
                 alt="Scam"
                 className="align-content-center"
               />
             </div>
             <div>
-              <button
-                className="btn btn-primary limit"
-                onClick={() =>activate(injected)}
-              >
+              <button className="btn btn-primary limit" onClick={connect}>
                 Assinatura
               </button>
             </div>
@@ -60,7 +63,7 @@ export function Home() {
             </p>
           </div>
         </div>
-        <div className="container">
+        <div className="container table-scroll scrollbar">
           <table className="table table-dark table-hover table-striped table-sm">
             <thead className="table-dark">
               <tr>
@@ -72,23 +75,40 @@ export function Home() {
               </tr>
             </thead>
             <tbody>
-              {balances.map((balance, index) => (
-                  <tr key={index}>
-                    <th scope="row">{index + 1}</th>
-                    <td
-                      className="select"
-                      onClick={(event) => copy(event.currentTarget.innerText)}
-                      value={index}
-                    >
-                      {balance.token.name}
-                    </td>
-                    <td value={index}>{balance.data}</td>
-                    <td value={index}>
-                      {" "}
-                      USD $ {Number(balance.price).toFixed(4)}
-                    </td>
-                  </tr>
-                ))}
+              {account
+                ? balances.map((balance, index) => (
+                      <tr key={index}>
+                        <th scope="row">{index + 1}</th>
+                        <td
+                          id={balance.token.name}
+                          className="select"
+                          onClick={(event) =>
+                            copy(event.currentTarget.innerText)
+                          }
+                          value={index}
+                        >
+                          {balance.token.name}
+                        </td>
+                        <td value={index}>{balance.data}</td>
+                        <td value={index}>
+                          {" "}
+                          USD $ {Number(balance.price).toFixed(4)}
+                        </td>
+                        <td>
+                          <a
+                            className=" btn btn-link btn-crypto"
+                            href={
+                              "https://poocoin.app/tokens/" +
+                              balance.token.address
+                            }
+                            target="_blank"
+                          >
+                            {balance.token.name}
+                          </a>
+                        </td>
+                      </tr>
+                    ))
+                : <span className="center">Wallet not connected</span>}
             </tbody>
           </table>
         </div>
